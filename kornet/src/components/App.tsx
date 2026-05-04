e0e7c40a121b70bc366e7abb2d480a7e379002df
Jacob Johansson <jacobknuts@gmail.com>
2026-05-04 20:40:24 +0200

    added very basic navbar that needs to be changed later, just needed to define page content

---
+++ kornet/src/components/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from './Navbar'
+import './styles/App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="main">
+        <h1>Welcome to Kornet</h1>
+        <p>Start building your amazing application here.</p>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ kornet/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Kornet</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Hem</a></li>
+        <li><a href="#about">Om Sofia</a></li>
+        <li><a href="#contact">Kontakt</a></li>
+        <li><a href="#treatments">Behandlingar</a></li>
+        <li><a href="#reviews">Omdömen</a></li>
+        <li><a href="#booking">Boka</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
